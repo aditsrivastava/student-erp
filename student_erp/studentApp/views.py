9cf6ec4e9da26eb35dd462331f5d748da4cb166e
Adit Srivastava <aditsriv@live.com>
2018-05-11 01:37:56 +0530

    Created the studentApp

---
+++ student_erp/studentApp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.contrib.auth import logout
+
+from login import views
+# Create your views here.
+
+def indexPage(request):
+	if request.user.is_authenticated:
+		if request.method=='POST':
+			logoutU = request.POST.get('logout')
+			if logoutU=='Logout':
+				logout(request)
+				return views.loginPage(request)
+
+		return render(request,'home/home.html',{'user':request.user})
+	else:
+		return views.loginPage(request)		
