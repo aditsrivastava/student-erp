34690b5fa65ae0e181a7634275d5a37bd63aa6fe
Adit Srivastava <aditsriv@live.com>
2018-05-13 02:08:26 +0530

    Created the full student model StudentData

--- student_erp/studentApp/models.py
+++ student_erp/studentApp/models.py
@@ -6,4 +6,19 @@ User = settings.AUTH_USER_MODEL
 
 class StudentData(models.Model):
 	name = models.ForeignKey(User)
-	dob = models.DateField()
+	rollNumber	= models.IntegerField()
+	dob 		= models.DateField()
+	email 		= models.EmailField()
+	contectNo	= models.CharField(max_length=10)
+	address		= models.CharField(max_length=150)
+	city		= models.CharField(max_length=40)
+	country		= models.CharField(max_length=40)
+
+	#Parents Details 
+	father_name			= models.CharField(max_length=100)
+	father_email		= models.EmailField()
+	father_contectNo	= models.CharField(max_length=10)
+
+	mother_name			= models.CharField(max_length=100)
+	mother_email		= models.EmailField()
+	mother_contectNo	= models.CharField(max_length=10)
