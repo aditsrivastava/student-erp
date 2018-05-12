8590027df7eb63f85cce079820cf75475a33cb0e
Adit Srivastava <aditsriv@live.com>
2018-05-12 19:28:40 +0530

    Created a demo model StudentData

--- student_erp/studentApp/models.py
+++ student_erp/studentApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class StudentData(models.Model):
+	name = models.ForeignKey(User)
+	dob = models.DateField()
